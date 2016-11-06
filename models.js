3583200257860bafe28dbb3f8d550692728edca3
Ritik <saxenar444@gmail.com>
2016-11-07 00:24:51 +0530

    minor change

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.db);
+mongoose.connect("mongodb://heroku_ggg8csx9:nqmddapnap0mpj0u4oj2lsq8hj@ds057806.mlab.com:57806/heroku_ggg8csx9");
 //var conn = mongoose.createConnection('mongodb://localhost/clipboard')
 
 //mongodb://localhost/clipboard
