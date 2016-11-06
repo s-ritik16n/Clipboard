9db1c2ed646251d6fea3c81955b00ed59aaf3868
Ritik <saxenar444@gmail.com>
2016-11-06 23:58:06 +0530

    security changes

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://heroku_ggg8csx9:nqmddapnap0mpj0u4oj2lsq8hj@ds057806.mlab.com:57806/heroku_ggg8csx9");
+var database = require('./confidential.js');
+mongoose.connect(database);
 //var conn = mongoose.createConnection('mongodb://localhost/clipboard')
 
 //mongodb://localhost/clipboard
